6bda42044397065b36b0d2af1a83da1c01723903
Artem <kardan97@yandex.ru>
2023-11-29 20:30:09 +0700

    Task 1 and Task 2 finished

--- Hometask_1/hometask1/src/main/java/ru/geekbrains/hometask1/Task2.java
+++ Hometask_1/hometask1/src/main/java/ru/geekbrains/hometask1/Task2.java
@@ -2,6 +2,8 @@ package ru.geekbrains.hometask1;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
 
 /**
  * 2. Создать класс Employee (Сотрудник) с полями: String name, int age, double salary, String department
@@ -30,8 +32,56 @@ public class Task2 {
         stuff.add(new Employee("Alena Sergeeva", 33,11000, "Marketing"));
         stuff.add(new Employee("Sergey Borisov", 50,12000, "Analytic"));
 
-        // 2.2 Вывести список всех различных отделов (department) по списку сотрудников
-        System.out.println(stuff.stream().filt;
+        System.out.println("Unique depts:");
+        getUniqueDepts(stuff);
+        System.out.println("----------");
+
+        System.out.println("Increase salary:");
+        salaryIncrease(stuff, 10000, 20);
+        System.out.println("----------");
+
+        System.out.println("Map employee:");
+        getMapEmployee(stuff);
+        System.out.println("----------");
+
+        System.out.println("Map salary:");
+        getMapSalary(stuff);
+        System.out.println("----------");
+    }
+
+    /**
+     * 2.2 Вывести список всех различных отделов (department) по списку сотрудников
+     */
+    private static void getUniqueDepts(List<Employee> stuff) {
+        stuff.stream().map(Employee::getDepartment).distinct().forEach(System.out::println);
+    }
+
+    /**
+     * 2.3 Всем сотрудникам, чья зарплата меньше 10_000, повысить зарплату на 20%
+     */
+    private static void salaryIncrease(List<Employee> stuff, int minSalary, int percent) {
+        stuff.stream().filter(s -> s.getSalary() < minSalary).forEach(s -> s.setSalary(s.getSalary() * (1 + (double)percent / 100)));
+        stuff.stream().forEach(System.out::println);
+    }
+
+    /**
+     * 2.4 * Из списка сотрудников с помощью стрима создать Map<String, List<Employee>> с отделами и сотрудниками внутри отдела
+     */
+    private static void getMapEmployee(List<Employee> stuff) {
+        Map<String, List<Employee>> mapEmployee =
+                stuff.stream().collect(Collectors.groupingBy(Employee::getDepartment));
+        mapEmployee.forEach((key, value) -> System.out.println("Department: " + key + "\nEmployees:" + value));
+    }
+
+    /**
+     * 2.5 * Из списка сорудников с помощью стрима создать Map<String, Double> с отделами и средней зарплатой внутри отдела
+     */
+    private static void getMapSalary(List<Employee> stuff) {
+        System.out.println("Department\t | \tAverage salary by dept");
+        Map<String, Double> mapSalary =
+                stuff.stream().collect(Collectors.groupingBy(Employee::getDepartment,
+                        Collectors.averagingDouble(Employee::getSalary)));
+        mapSalary.forEach((key, value) -> System.out.println(key + "\t | \t" + value));
     }
 
 }
