8b9759c214adc3cad6d46582af40ef83b9693381
Artem <kardan97@yandex.ru>
2023-12-12 00:00:16 +0700

    Task 1 finished

--- Hometask_3/Hometask3/src/main/java/ru/geekbrains/hometask3/Program.java
+++ Hometask_3/Hometask3/src/main/java/ru/geekbrains/hometask3/Program.java
@@ -4,6 +4,14 @@ public class Program {
 
     public static void main(String[] args) {
 
+        Phonebook phonebook = new Phonebook();
+        phonebook.addContact(new Contact("Ivanov Ivan","8(999)8887766", "ivanov_ivan@mail.ru"));
+        phonebook.addContact(new Contact("Petrov Petr","8(999)7776655", "petrov_petr@mail.ru"));
+        phonebook.addContact(new Contact("Sergeev Sergey","8(999)6665544", "sergeev_sergey@mail.ru"));
+
+        SerializeObjectToFile serializeObjectToFile = new SerializeObjectToFile();
+        serializeObjectToFile.writeObjectToFile(phonebook);
+        serializeObjectToFile.readObjectFromFile(serializeObjectToFile.getPath());
     }
-    
+
 }
